6e8a2a0b3c99e81e6d52557bcc61224735520af8
jennifer <cxxyao2@gmail.com>
2025-04-29 20:13:59 -0400

    test2

--- EclinicBackend/Program.cs
+++ EclinicBackend/Program.cs
@@ -1,9 +1,21 @@
-
-
+using EclinicBackend;
+using EclinicBackend.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+
+builder.Services.AddHttpContextAccessor();
+var connectionString = builder.Configuration.GetConnectionString("PostgresConnection") ?? throw new InvalidOperationException("Connection string 'PostgresConnection' not found.");
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(connectionString));
+
+
+builder.Services.AddAutoMapper(typeof(MappingProfile));
+
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
